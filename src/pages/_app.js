1f8f9f180d4e0d271dd3cee7e25c5bcc130f63ee
Lucas Prazeres <olucascostadp@gmail.com>
2022-03-09 22:21:17 -0300

    feat: setup landing page

---
+++ src/pages/_app.js
@@ -0,0 +1,9 @@
+import styles from '../styles/global.css'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  )
+}
+
+export default MyApp
---
+++ src/pages/index.js
@@ -0,0 +1,40 @@
+import Image from "next/image"
+import Link from "next/link"
+import { FiArrowRight } from 'react-icons/fi'
+import logoImg from '../../public/assets/logo.svg'
+
+import styles from '../styles/Home.module.css'
+
+export default function Landing() {
+  return (
+    <div className={styles.landing}>
+      <div className={styles.contentWrapper}>
+        <header>
+          <Image src={logoImg} alt="Happy" />
+
+          <div className={styles.location}>
+            <strong>Belém</strong>
+            <p>Pará</p>
+          </div>
+
+          <Link href="/dashboard">
+          <a className={styles.enterDashboard}>
+            Acesso restrito
+          </a>
+        </Link>
+        </header>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia <br/> de muitas crianças.</p>
+        </main>
+
+        <Link href="/app">
+          <a className={styles.enterApp}>
+            <FiArrowRight  size={26} color="rgba(0, 0, 0, 0.6)"/>
+          </a>
+        </Link>
+      </div>
+    </div>
+  )
+}
