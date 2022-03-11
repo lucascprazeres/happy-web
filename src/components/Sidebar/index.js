0c5948042c6232dcb401f0647f43c2b4103b42c8
Lucas Prazeres <olucascostadp@gmail.com>
2022-03-11 12:42:53 -0300

    feat: add regular sidebar component

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import { FiArrowLeft } from 'react-icons/fi';
+
+import mapMarkerImg from '../../../public/assets/map-marker.svg'
+
+import styles from './styles.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.container}>
+      <Image src={mapMarkerImg} alt="Happy"/>
+
+      <Link href="/app">
+        <a className={styles.goBackButton}>
+          <FiArrowLeft size={24} color="#fff" />
+        </a>
+      </Link>
+    </aside>
+  )
+}
--- src/pages/orphanage/[id].js
+++ src/pages/orphanage/[id].js
@@ -1,10 +1,15 @@
 import { useRouter } from "next/router"
+import { Sidebar } from "../../components/Sidebar"
+
+import styles from './styles.module.css'
 
 export default function Orphanage() {
   const router = useRouter()
   const { id } = router.query
   
   return (
-    <h1>Orfanato {id}</h1>
+    <div className={styles.container}>
+      <Sidebar />
+    </div>
   )
 }
